cfb95a4c5de18deabd0a4cc84a479cb209ccf067
binh <khanhbinh1186@gmail.com>
2025-06-02 20:55:45 +0700

    validate dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,17 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
 export class CreateUserDto {
+    @IsEmail({},{
+        message:'Email khong dung dinh dang'
+    })
+    @IsNotEmpty({
+        message:'Email ko dc de trong'
+    })
     email:string;
+
+    @IsNotEmpty({
+        message:"Password ko dc de trong"
+    })
     password:string;
+
     name:string;
 }
